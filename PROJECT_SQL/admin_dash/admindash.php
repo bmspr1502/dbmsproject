198156dbc227bdf74488dd34ebd2bfc82fc0194b
Sugavaneshwaran K <53948874+Sugavanesh10@users.noreply.github.com>
2021-05-26 22:41:47 +0530

    admin dashboard

--- PROJECT_SQL/admin_dash/admindash.php
+++ PROJECT_SQL/admin_dash/admindash.php
@@ -11,58 +11,69 @@ if(!isset($_SESSION['aid']))
 <!DOCTYPE html>
 <html>
 <head>
-     <!-- Required meta tags -->
-     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <!-- Bootstrap CSS -->
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" >
-    <link href="../bootstrap/css/stddashstyle.css" rel="stylesheet" >
-<link href="../fontawesome-free-5.15.3-web/css/all.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="style.css">
+
 </head>
 <body>
-<div id="topheader">
-    <div class="container-fluid">
-	
-	   
-	
-       <h2>CEG COURSE MANAGEMENT</h2> 
-       
-    </div>
-</div>
-<div class="container-fluid" id="sidebar" >
-    <div class="row" >
-        
-        <h1>HELLO ADMIN</h1>
-        
-            <ul  >
-                <a href="stddash.php">
-				<li>DASHBOARD</li>
-                </a>
-               <a href="add_new_student.php">
-                <li>ADD NEW STUDENT</li></a>
-                
-                <a href="add_new_prof.php">
-                <li>ADD NEW PROFESSOR</li>
-                </a>
-				<a href="add_new_course.php">
-                <li>ADD NEW COURSE</li></a>
-				<a href="view_student_request.php">
-                <li>VIEW STUDENTS REQUESTS</li></a>
-                <a href="student_update_details.php">
-                <li>SEND NOTIFICATIONS</li>
-                </a>
-				<a href="log_out.php">
-                <li>LOG OUT</li>
-                </a>
-             
 
-            </ul>
-            
-    
-        </div>
 
-    </div>
+
+		<div class="wrapper d-flex align-items-stretch">
+			<div class="wrapper d-flex align-items-stretch">
+			<nav id="sidebar" class="active">
+				<h1><a href="#" class="logo">CMS</a></h1>
+        <ul class="list-unstyled components mb-5">
+          <li class="active">
+            <a href="admindash.php"><span class="fa fa-home"></span>DASHBOARD</a>
+          </li>
+		  <li>
+            <a href="admin_profile_details.php"><span class="fa fa-user"></span>PROFILE DETAILS</a>
+          </li>
+          <li>
+            <a href="add_new_student.php"><span class="fa fa-user-plus"></span>ADD NEW STUDENT</a>
+          </li>
+          <li>
+            <a href="add_new_prof.php"><span class="fa fa-user-plus"></span>ADD NEW PROFESSOR</a>
+          </li>
+          <li>
+            <a href="add_new_course.php"><span class="fa fa-upload"></span>ADD NEW COURSE</a>
+          </li>
+          <li>
+            <a href="view_student_request.php"><span class="fa fa-newspaper-o"></span>VIEW STUDENTS REQUESTS</a>
+          </li>
+          <li>
+            <a href="anotifications.php"><span class="fa fa-paper-plane"></span>SEND NOTIFICATIONS</a>
+          </li>
+          <li>
+            <a href="logout.php"><span class="fa fa-sign-out"></span>LOG OUT</a>
+          </li>
+        </ul>
+    	</nav>
+		<div id="content" class="p-4 p-md-5">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+
+            <button type="button" id="sidebarCollapse" class="btn btn-primary">
+              <i class="fa fa-bars"></i>
+              <span class="sr-only">Toggle Menu</span>
+            </button>
+            <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+            </button>
+			</div>
+        </nav>
+
+
     <div class="container-fluid p-0" id="con">
         <div class="content">
             <h3>Profile Details</h3>
@@ -170,14 +181,10 @@ if(!isset($_SESSION['aid']))
     </div>
     </div>
 </div>
-   <script src="bootstrap/js/jquery-3.3.1.slim.min.js"></script>
-    <script src="bootstrap/js/popper.min.js"></script>
-    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</div>
+
+	<script src="main.js"></script>
 
 </body>
-
-
-
-
 </html>
 
