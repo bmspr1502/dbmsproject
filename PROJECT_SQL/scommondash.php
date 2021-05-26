2d730d9d62cfd354f242fcd9fa7560dc9ba211a1
DHEEPAKRAAJ123 <rdheepakraaj123@gmail.com>
2021-05-26 15:14:38 +0530

    Update scommondash.php

--- PROJECT_SQL/scommondash.php
+++ PROJECT_SQL/scommondash.php
@@ -15,11 +15,9 @@ if(!isset($_SESSION['sroll_no']))
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="../style.css">
+		<link rel="stylesheet" href="style.css">
 
   </head>
   <body>
@@ -79,10 +77,10 @@ if(!isset($_SESSION['sroll_no']))
        
       </div>
 		</div>
-
-    <script src="../jquery.min.js"></script>
-    <script src="../popper.js"></script>
-    <script src="../bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+   
     <script src="../main.js"></script>
   </body>
 </html>
